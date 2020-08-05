fd6f64a17dadbc043be0a2645f9e60ccc9b637a3
rutik wankhade <rutikwankhade2@gmail.com>
2020-08-05 15:40:14 +0530

    created a coverimg component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import CoverImage from './CoverImage';
+
 
 class App extends React.Component{
     
     
     render(){
-        return <div>App</div>;
+        return (
+        
+            <CoverImage/>
+            
+            
+            );
 
     }
 }
---
+++ src/components/CoverImage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './CoverImage.css';
+
+
+class CoverImage extends React.Component{
+
+
+    render(){
+        return (
+            <div className="cover img-fluid">
+             <div className="card">
+             <h2>5 Simple apps I built while learning JavaScript</h2>
+            </div>
+            </div>
+        );
+    }
+}
+
+export default CoverImage;
