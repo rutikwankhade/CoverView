f1bbb461ea9fa5da578f7eddb166db01e56fff6b
rutik wankhade <rutikwankhade2@gmail.com>
2020-08-17 11:12:16 +0530

    fix:added gihub link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import logo from '../assets/favicon.png';
+import githubIcon from '../assets/github.png';
 const Navbar = () => {
 
     return (
-    <nav className="navbar bg-dark">
-        
-        <h4><img src={logo} alt="logo" className="logo"></img>CoverView</h4></nav>);
+        <nav className="navbar bg-dark">
+
+            <h4><img src={logo} alt="logo" className="logo"></img>CoverView</h4>
+            <a href="https://github.com/rutikwankhade/CoverView">
+                <img src={githubIcon} className="logo" alt="giticon"></img>
+            </a>
+        </nav>);
 
 
 
