7a559112021f69822e6fef7ebdfe8833c23b4355
AsishRaju <41658324+AsishRaju@users.noreply.github.com>
2020-08-18 18:56:59 +0530

    More Customizable Options

--- src/components/ComponentToImg.js
+++ src/components/ComponentToImg.js
@@ -1,36 +1,33 @@
-import React from 'react';
+import React from "react";
 import { exportComponentAsJPEG, exportComponentAsPNG } from "react-component-export-image";
-import './CoverImage.css'
-
-
+import "./CoverImage.css";
 
 class ComponentToImg extends React.Component {
-    constructor(props) {
-        super(props);
-        this.componentRef = React.createRef();
-    }
+	constructor(props) {
+		super(props);
+		this.componentRef = React.createRef();
+	}
 
-    render() {
-        console.log(this.props.children);
+	render() {
+		console.log(this.props.children);
+		let downloadButton;
+		switch (this.props.downloadAs) {
+			case "PNG":
+				downloadButton = <button onClick={() => exportComponentAsPNG(this.componentRef)}>Download</button>;
+				break;
+			case "JPEG":
+				downloadButton = <button onClick={() => exportComponentAsJPEG(this.componentRef)}>Download</button>;
+		}
+		console.log(downloadButton);
 
-        return (
-            <React.Fragment>
-
-                <div ref={this.componentRef}>
-                    {this.props.children}
-                </div>
-
-                    {/* <button onClick={() => exportComponentAsJPEG(this.componentRef)}>
-                        Export As JPEG
-                    </button> */}
-
-                    <button onClick={() => exportComponentAsPNG(this.componentRef)}>
-                        Download
-                     </button>
-                
-            </React.Fragment>);
-    }
-
+		return (
+			<React.Fragment>
+				<div ref={this.componentRef}>{this.props.children}</div>
+				{/* <button onClick={() => exportComponentAsPNG(this.componentRef)}>Download</button> */}
+				{downloadButton}
+			</React.Fragment>
+		);
+	}
 }
 
 export default ComponentToImg;
--- src/components/CoverImage.js
+++ src/components/CoverImage.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import './CoverImage.css';
+import React from "react";
+import "./CoverImage.css";
+import "../assets/css/patterns.css";
 
 class CoverImage extends React.Component {
+	hexToRgbA(hex, opacity) {
+		var c;
+		if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+			c = hex.substring(1).split("");
+			if (c.length == 3) {
+				c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+			}
+			c = "0x" + c.join("");
+			return "rgba(" + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(",") + `,${opacity})`;
+		}
+		throw new Error("Bad Hex");
+	}
 
-    render() {
-        return (
-            <div className={` cover ${this.props.pattern} `}
-                style={{ backgroundColor: this.props.bgcolor }}>
-                <div className={`${this.props.pattern}`}>
-                    <div className="title-card"
-                        style={{ borderTop: `18px solid ${this.props.border}` }}>
-                        <p>{this.props.title}</p>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		const { title, bgColor, borderColor, borderSize, borderType, pattern, foregroundColor, textColor, opacity } = this.props;
+		const styleToApply = {
+			borderTop: `${borderSize}px ${borderType} ${borderColor}`,
+			backgroundColor: `${this.hexToRgbA(foregroundColor, opacity)}`,
+			color: `${textColor}`,
+		};
+		return (
+			<div className={` cover ${pattern} `} style={{ backgroundColor: bgColor }}>
+				<div className={`${pattern}`}>
+					<div className="title-card" style={styleToApply}>
+						<p>{title}</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default CoverImage;
