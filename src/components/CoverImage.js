f1a99caa25a26fea7d2102ce1037ef617e0bb171
rutik wankhade <rutikwankhade2@gmail.com>
2022-01-07 15:32:46 +0530

    style: update blog cover styling

--- src/components/CoverImage.js
+++ src/components/CoverImage.js
@@ -3,39 +3,40 @@ import "./CoverImage.css";
 import "../assets/css/patterns.css";
 
 class CoverImage extends React.Component {
-	hexToRgbA(hex, opacity) {
-		var c;
-		if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
-			c = hex.substring(1).split("");
-			if (c.length === 3) {
-				c = [c[0], c[0], c[1], c[1], c[2], c[2]];
-			}
-			c = "0x" + c.join("");
-			return "rgba(" + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(",") + `,${opacity})`;
-		}
-		throw new Error("Bad Hex");
-	}
+	// hexToRgbA(hex, opacity) {
+	// 	var c;
+	// 	if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+	// 		c = hex.substring(1).split("");
+	// 		if (c.length === 3) {
+	// 			c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+	// 		}
+	// 		c = "0x" + c.join("");
+	// 		return "rgba(" + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(",") + `,${opacity})`;
+	// 	}
+	// 	throw new Error("Bad Hex");
+	// }
 
 	render() {
-		const { title, bgColor, borderColor, borderSize, borderType, pattern, foregroundColor, textColor, opacity, author, icon } = this.props;
-		const styleToApply = {
-			borderTop: `${borderSize}px ${borderType} ${borderColor}`,
-			backgroundColor: `${this.hexToRgbA(foregroundColor, opacity)}`,
-			color: `${textColor}`,
-		};
+		const { title, bgColor, pattern, author, icon } = this.props;
+	
 		return (
 			<div>
 				<div className={` cover ${pattern} `} style={{ backgroundColor: bgColor }}>
 					<div className={`${pattern}`}>
-						<div className="title-card" style={styleToApply}>
-							<p>{title}</p>
+
+						<div className="title-card">
+							<div className="flex-row">
+								<i className={`devicon-${icon.value}-plain dev-icon `}></i>
+								<div>
+									<h1 className="title">{title}</h1>
+									<h2 className="author-text"> {author}</h2>
+								</div>
+							</div>
 						</div>
 
 					</div>
 				</div>
-				<div className="meta" style={{ backgroundColor: borderColor }}>
-					<i className={`devicon-${icon.value}-plain dev-icon`}></i>
-					<span className="author">A blog by  <span role="img" aria-label="bolt">⚡</span> {author}</span></div>
+				
 			</div>
 		);
 	}
