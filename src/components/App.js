157d78c6540c0cd67e143892b286598cf8e94c0f
rutik wankhade <rutikwankhade2@gmail.com>
2022-04-25 13:06:21 +0530

    feat: add faq section

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Editor from './Editor';
-
+import Info from './Info'
 class App extends React.Component {
 
     render() {
         return (
             <div className="content ">
                 <Editor />
+                <Info/>
             </div>
         );
     }
